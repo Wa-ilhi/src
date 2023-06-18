d5e22c5c917d52cd47e0c80d7f7b367204284647
Wa-ilhi <128329982+Wa-ilhi@users.noreply.github.com>
2023-06-18 16:17:52 +0800

    Update App.js

--- App.js
+++ App.js
@@ -3,15 +3,23 @@ import Login from "./components/Login";
 import Homepage from "./components/Homepage";
 import Logout from "./components/Logout";
 import { Protect } from "./help";
+import { ToastContainer } from "react-toastify";
+import Registration from "./components/Registration";
+import RecipeInfo from "./components/Homepage/RecipeInfo";
+
 function App() {
   return (
     <div>
+     
       <BrowserRouter>
       <Routes>
         <Route path= "/" element = {<Protect Component={Homepage}/>} />
+        <Route path = "/:recipeId" element = {<RecipeInfo/>}/>
         <Route path= "/login" element = {<Login/>} />
         <Route path= "/logout" element = {<Logout/>} />
+        <Route path= "/register" element = {<Registration/>} />
       </Routes>
+      <ToastContainer/>
       </BrowserRouter>
     </div>
   );
